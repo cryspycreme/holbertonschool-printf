5b55187e5f25b02345a0782f79b4d7babe9c4121
“cryspycreme” <11529@holbertonstudents.com>
2025-07-23 02:21:49 +0000

    Update betty doc for printf_specifiers

--- printf_specifiers.c
+++ printf_specifiers.c
@@ -2,19 +2,34 @@
 #include <string.h> /*strlen*/
 #include <unistd.h> /*write*/
 
+/**
+ * print_char - prints a character
+ *
+ * @character: character to be printed
+ * Return: no. bytes written.
+ */
+
 int print_char(char character)
 {
-    int c;
-    c = write(1, &character, 1);
-    return (c); /*as write prints but also returns the no. of bytes*/
+	int c;
+
+	c = write(1, &character, 1);
+	return (c); /*as write prints but also returns the no. of bytes*/
 }
+
+/**
+ * print_string - prints a string
+ *
+ * @string: string to be printed
+ * Return: no. bytes written.
+ */
 
 int print_string(const char *string)
 {
-    int s;
+	int s;
 
-    if (string == NULL)
-        string = "(null)";
-    s = write(1, string, strlen(string));
-    return (s);
+	if (string == NULL)
+		string = "(null)";
+	s = write(1, string, strlen(string));
+	return (s);
 }
